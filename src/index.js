143b23018c93c2dd83463bd357f5d8e0f05a6109
Erika Carolina Alvarado Morales <75351106+Carolina-Alvarado@users.noreply.github.com>
2023-07-10 11:32:03 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import app from './server.js'
+import connection from './database.js';
+
+
+connection()
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port ${app.get('port')}`);
+})
